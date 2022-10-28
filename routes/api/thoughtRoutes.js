d38f9ccf22cafc211c11a67dcfecdc6052a258cb
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-28 01:14:00 -0700

    updated thoughtRoutes.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addThoughtReaction,
+  removeThoughtReaction,
+} = require('../../controllers/ThoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+//   /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addThoughtReaction);
+
+// /api/thoughtId/reactions/;reactionId').delete(removeThoughtReaction);
+router.route('/:thoughtId/reactions/;reactionId').delete(removeThoughtReaction);
+
+module.exports = router;
